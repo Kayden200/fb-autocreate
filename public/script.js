17c0ad9fb8e2a0871b85c9c6e5379e0a788b5969
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2026-05-11 02:42:09 +0800

    Add event listener to start button for account creation

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
+document.getElementById('startBtn').addEventListener('click', async () => {
+    const con = document.getElementById('console');
+    con.innerText = "Launching Browser...";
+    
+    const res = await fetch('/create-account');
+    const data = await res.json();
+    
+    if (data.success) {
+        con.innerText = `Success! Code: ${data.code}`;
+    } else {
+        con.innerText = `Error: ${data.error}`;
+    }
+});
 
